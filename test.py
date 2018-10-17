7fc774d7854cc37fe5dd0d6fc38f79f882561b3e
ShiruiStan <34876230+ShiruiStan@users.noreply.github.com>
2018-10-17 11:36:23 +0800

    Revert "Update test.py"
    This reverts commit f12abd6760174f2de0dfca89ff61b05091969cf3.

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 ATCDI = "HelloWorld"
 
+<<<<<<< HEAD
 #version = "1.0"
 
-version = "2.0"
+#version = "2.0"
+
+version = "3.0"
+=======
+version = "1.0"
+>>>>>>> parent of 35e2c69... Update test.py
