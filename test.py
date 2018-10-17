e370c41159749e2e3112d5a1e7e0a8337438d550
ShiruiStan <34876230+ShiruiStan@users.noreply.github.com>
2018-10-17 12:19:53 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-ATCDI = "HelloWorld"
-
-<<<<<<< HEAD
 #version = "1.0"
 
-#version = "2.0"
-
-version = "3.0"
-=======
-version = "1.0"
->>>>>>> parent of 35e2c69... Update test.py
+version = "2.0"
